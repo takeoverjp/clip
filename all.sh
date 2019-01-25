5d808f6b1432ae4847233476a5a8ecb667636a76
Kondo Takeo <takeoverjp@gmail.com>
2019-01-25 17:39:51 +0900

    Create mnist data.

--- all.sh
+++ all.sh
@@ -5,3 +5,7 @@ do
     outfile=${infile%.jpg}_clipped.raw
     ./clip.py $infile $outfile
 done
+
+rm -f data/hdr.mnist data/images.mnist
+dd if=/dev/zero of=data/hdr.mnist count=16 bs=1
+cat data/hdr.mnist data/*_clipped.raw > data/images.mnist
